01950dfe781c842e2b0fdaff7a8a8c957390342c
terman37 <terman37@yahoo.fr>
2020-02-13 14:19:35 +0100

    Update storedprocedure_pythonway.py

--- SurveySample/storedprocedure_pythonway.py
+++ SurveySample/storedprocedure_pythonway.py
@@ -1,11 +1,15 @@
 
 import pandas as pd
 import pyodbc
+import time 
 
 sql_conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER=LAPTOP-NLGPTOS3;DATABASE=Survey_Sample_A18;Trusted_Connection=yes')
 
-# template query definitions
 
+### first way using same structure as in stored procedure in MSSQL
+
+# template query definitions
+t1=time.time()
 qry_TemplateForAnswerColumn = 'COALESCE( ' \
 				+ '(SELECT a.Answer_Value ' \
 				+ 'FROM	Answer as a ' \
@@ -14,7 +18,7 @@ qry_TemplateForAnswerColumn = 'COALESCE( ' \
 
 qry_TemplateForNullColumn = 'NULL As ANS_Q<QUESTION_ID> '
 
-qry_TemplateOuterUnionQuery = 'SELECT u.UserId, u.User_Name, <SURVEY_ID> as SurveyId, <DYNAMIC_QUESTION_ANSWERS> ' \
+qry_TemplateOuterUnionQuery = 'SELECT u.UserId, <SURVEY_ID> as SurveyId, <DYNAMIC_QUESTION_ANSWERS> ' \
 				+ 'FROM	[User] as u ' \
 				+ 'WHERE EXISTS( ' \
 				+ 'SELECT * FROM Answer as a WHERE a.UserId=u.UserId AND a.SurveyId = <SURVEY_ID>) '
@@ -58,7 +62,36 @@ for surveyid in surveys['SurveyId']:
 	finalQueryList.append(currentUnionQueryBlock)
 
 finalQuery = 'UNION '.join(finalQueryList)
-
 finalResult = pd.read_sql(finalQuery, sql_conn)
+
+t2=time.time()
+print("final result v1: (execution time:%.4f secs)" % (t2-t1))
 print(finalResult.head())
+print(finalResult.tail())
 print(finalResult.info())
+print("\n\n")
+
+
+### second way, more direct python way. maybe less flexible... exact same result (slighty slower execution)
+t1=time.time()
+# Create pivot of answers vs userid and surveyid
+qry_answers = "SELECT UserId,SurveyId,CONCAT('ANS_Q',QuestionId) as Question,Answer_Value FROM Answer"
+df_answers = pd.read_sql(qry_answers,sql_conn)
+p_df_answers = df_answers.pivot_table(index=['UserId','SurveyId'],columns='Question',values='Answer_Value').reset_index()
+
+# if question exists is survey structure, it means user has not answered --> replace NaN by -1
+qry_surveystructure = 'SELECT SurveyId, QuestionId From SurveyStructure'
+df_surveystructure = pd.read_sql(qry_surveystructure,sql_conn)
+for idx,struct in df_surveystructure.iterrows():
+    surveyid = struct['SurveyId']
+    questionid = struct['QuestionId']
+    p_df_answers_filled = p_df_answers.loc[p_df_answers['SurveyId']==surveyid,'ANS_Q'+str(questionid)].fillna(-1)
+    p_df_answers.loc[p_df_answers['SurveyId']==surveyid,'ANS_Q'+str(questionid)] = p_df_answers_filled
+
+finalResult2 = p_df_answers.sort_values(by=['SurveyId','UserId'])
+t2=time.time()
+
+print("final result v2: (execution time:%.4f secs)" % (t2-t1))
+print(finalResult2.head())
+print(finalResult2.tail())
+print(finalResult2.info())
